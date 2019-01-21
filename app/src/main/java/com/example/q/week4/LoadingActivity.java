50e7e71dfc4724909d88293c492929675912d644
sangeuni <goakgoak1995@gmail.com>
2019-01-21 22:20:24 +0900

    first commit

---
+++ app/src/main/java/com/example/q/week4/LoadingActivity.java
@@ -0,0 +1,33 @@
+package com.example.q.week4;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        LottieAnimationView lottieAnimationView = (LottieAnimationView)findViewById(R.id.lottie);
+        lottieAnimationView.setAnimation("dog.json");
+        lottieAnimationView.loop(true);
+        lottieAnimationView.playAnimation();
+        startLoading();
+
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },5000);
+    }
+}
+
